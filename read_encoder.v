5c37b12b96a615d4267e69fbd824f6df28f34181
KaioMenezesB <kaio_borduque@hotmail.com>
2025-05-07 14:38:48 -0300

    Atividade Finalizada

--- read_encoder.v
+++ read_encoder.v
@@ -7,5 +7,29 @@ module Read_Encoder (
 
     output reg [1:0] dir
 );
-    
+    reg [1:0] prev;  
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            prev <= 2'b00;
+            dir  <= 2'b00;
+        end else begin
+            case ({prev, A, B})
+                4'b00_10,
+                4'b10_11,
+                4'b11_01,
+                4'b01_00: dir <= 2'b01;  
+
+                4'b00_01,
+                4'b01_11,
+                4'b11_10,
+                4'b10_00: dir <= 2'b10;  
+
+                default:  dir <= 2'b00;  
+            endcase
+
+            prev <= {A, B};
+        end
+    end
+
 endmodule
